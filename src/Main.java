b080717fad244a906c9277206568ca318310be7b
Birlutiu Claudiu-Andrei <claudiu.birlutiu@fortech.ro>
2023-07-25 19:19:29 +0300

    TASK-23: message from task 23 Mihail

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("Hello from main");
         System.out.println("Hello from task 22");
         System.out.println("Hello from task 23 correct move");
-        System.out.println("Hello, Mihai!");
+        System.out.println("Hello, Mihail!");
         System.out.println("Hello from task 23");
         System.out.println("Hello from main 25");
     }
