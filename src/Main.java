44a73ac7764e57fca2d789619c2faeefb62f07d7
Birlutiu Claudiu-Andrei <claudiu.birlutiu@fortech.ro>
2023-07-25 18:40:35 +0300

    TASK-23: add nee message

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello from main");
         System.out.println("Hello from task 22");
+        System.out.println("Hello from task 23");
     }
 }
